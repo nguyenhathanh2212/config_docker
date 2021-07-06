11428e861b344a08278ae5af4c5ff665d9a7e727
ThanhNguyen <thanhnguyen@ThanhNguyen.mshome.net>
2021-07-07 02:23:55 +0700

    Init master

---
+++ src/be/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+
+const app = express()
+app.use(cors());
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+async function connectMongo() {
+    const uri = "mongodb://mongo:27017";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+connectMongo().catch(console.error);
